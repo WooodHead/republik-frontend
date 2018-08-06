b5bbd5175b3a90e7082e9da5ee122a0da8c26d6b
Thomas Preusse <tpreusse@users.noreply.github.com>
2018-08-06 08:22:00 +0200

    poller refactor

--- components/Auth/Poller.js
+++ components/Auth/Poller.js
@@ -1,7 +1,22 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import PropTypes from 'prop-types'
 import { graphql, compose } from 'react-apollo'
 import { meQuery } from '../../lib/apollo/withMe'
+
+import withT from '../../lib/withT'
+
+import ErrorMessage from '../ErrorMessage'
+
+import { SUPPORTED_TOKEN_TYPES } from '../constants'
+
+import {
+  Label,
+  Interaction,
+  RawHtml,
+  linkRule
+} from '@project-r/styleguide'
+
+const { H2, P } = Interaction
 
 class Poller extends Component {
   constructor (props) {
@@ -42,23 +57,88 @@ class Poller extends Component {
     clearTimeout(this.tickTimeout)
   }
   render () {
-    const {data: {error, me}} = this.props
+    const { data: { error, me } } = this.props
     if (me) {
       return null
     }
 
-    return (
-      <span>
-        {!!error && <span><br />{error.toString()}</span>}
-      </span>
-    )
+    if (error) {
+      return <ErrorMessage error={error} />
+    }
+
+    const {
+      tokenType,
+      email,
+      onCancel,
+      phrase,
+      alternativeFirstFactors,
+      onTokenTypeChange,
+      t
+    } = this.props
+
+    return (<Fragment>
+      <H2>{t(`signIn/polling/${tokenType}/title`)}</H2>
+      <RawHtml
+        type={P}
+        dangerouslySetInnerHTML={{
+          __html: t(`signIn/polling/${tokenType}/text`)
+        }}
+      />
+      {!!onTokenTypeChange && alternativeFirstFactors.map(altTokenType => (
+        <P key={altTokenType}>
+          <a {...linkRule}
+            href='#'
+            onClick={(e) => {
+              e.preventDefault()
+              onTokenTypeChange(altTokenType)
+            }}
+          >
+            {t(`signIn/polling/switch/${altTokenType}`)}
+          </a>
+        </P>
+      ))}
+      <P>
+        <Label>{t('signIn/polling/phrase')}</Label><br />
+        {phrase}
+      </P>
+      <P>
+        <Label>{t('signIn/polling/email')}</Label><br />
+        {email}
+      </P>
+      {!!onCancel && (
+        <P>
+          <a {...linkRule}
+            href='#'
+            onClick={(e) => {
+              e.preventDefault()
+              onCancel()
+            }}
+          >
+            {t('signIn/polling/cancel')}
+          </a>
+        </P>
+      )}
+    </Fragment>)
   }
 }
 
 Poller.propTypes = {
-  onSuccess: PropTypes.func
+  tokenType: PropTypes.oneOf(SUPPORTED_TOKEN_TYPES).isRequired,
+  email: PropTypes.string.isRequired,
+  phrase: PropTypes.string.isRequired,
+  alternativeFirstFactors: PropTypes.arrayOf(
+    PropTypes.oneOf(SUPPORTED_TOKEN_TYPES)
+  ).isRequired,
+  onSuccess: PropTypes.func,
+  onCancel: PropTypes.func,
+  onTokenTypeChange: PropTypes.func
+}
+
+Poller.defaultProps = {
+  alternativeFirstFactors: []
 }
 
 export default compose(
-  graphql(meQuery)
+  graphql(meQuery),
+  withT
 )(Poller)
